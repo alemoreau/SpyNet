d890d2817df1d2f4b26a04ce1951c69de0be6d71
Alemoreau <alemoreau@enseirb-matmeca.fr>
2015-12-27 01:59:19 +0100

    Model in progress

--- SpyNet/game/models.py
+++ SpyNet/game/models.py
@@ -18,7 +18,6 @@ class Player(models.Model):
     techno_antispyware = models.SmallIntegerField()
     techno_virus = models.SmallIntegerField()
     techno_antivirus = models.SmallIntegerField()
-    techno_ranconware = models.SmallIntegerField()
     techno_scan = models.SmallIntegerField()
     techno_proxy = models.SmallIntegerField()
     techno_firewall = models.SmallIntegerField()
@@ -41,14 +40,12 @@ class Computer(models.Model):
     data = models.IntegerField()
 
     # Hardware
-    cooling_system = models.SmallIntegerField('CoolingSystem')
-    power_supply = models.SmallIntegerField('PowerSupply')
-    network_interface = models.SmallIntegerField('NetworkInterface')
-    cpu = models.SmallIntegerField('CPU')
-    core_count = models.SmallIntegerField()
-    ram = models.SmallIntegerField('RAM')
-    memory_stick = models.SmallIntegerField('MemoryStick')
-    hard_drive = models.SmallIntegerField('HardDrive')
+    cooling_system = models.ForeignKey('CoolingSystem')
+    power_supply = models.ForeignKey('PowerSupply')
+    network_interface = models.ForeignKey('NetworkInterface')
+    cpu = models.ForeignKey('CPU')
+    ram = models.ForeignKey('RAM')
+    hard_drive = models.ForeignKey('HardDrive')
     
     # Software
     anti_virus = models.SmallIntegerField()
@@ -72,7 +69,7 @@ class Computer(models.Model):
 
     date_creation = models.DateTimeField(auto_now_add=True, auto_now=False,
                                          verbose_name="Creation date")
-    date_update = models.DateTimeField(auto_now_add=True, auto_now=True,
+    date_update = models.DateTimeField(auto_now_add=False, auto_now=True,
                                        verbose_name="Update date")
 
     def __str__(self):
@@ -81,10 +78,12 @@ class Computer(models.Model):
 
 class Process(models.Model):
     name = models.CharField(max_length=10)
-    host = models.ForeignKey('Computer')
-    owner = models.ForeignKey('Computer')
+    host = models.ForeignKey('Computer',
+                             related_name='running_processes')
+    owner = models.ForeignKey('Computer',
+                              related_name='owned_processes')
     level = models.SmallIntegerField()
-    activated = models.SmallBooleanField()
+    activated = models.BooleanField()
     kind = models.SmallIntegerField()
 
     def __str__(self):
@@ -92,9 +91,106 @@ class Process(models.Model):
 
     
 class Link(models.Model):
-    computer_from = models.Foreign('Computer')
-    computer_to = models.Foreign('Computer')
+    computer_from = models.ForeignKey('Computer',
+                                      related_name='computer_to')
+    computer_to = models.ForeignKey('Computer',
+                                    related_name='computer_from')
     activated = models.BooleanField()
-
+    secured = models.BooleanField()
+    visible = models.BooleanField()
+    
     def __str__(self):
         return "Link"
+
+
+class PowerSupply(models.Model):
+    description = models.TextField
+    price = models.SmallIntegerField()
+    image = models.ImageField()
+    power_max = models.SmallIntegerField()
+    level = models.SmallIntegerField()
+    upgrade = models.OneToOneField('PowerSupply',
+                                   related_name='downgrade')
+
+    def __str__(self):
+        return "Power supply : " + self.description
+
+
+class CoolingSystem(models.Model):
+    description = models.TextField()
+    price = models.SmallIntegerField()
+    image = models.ImageField()
+    power_max = models.SmallIntegerField()
+    cooling_max = models.SmallIntegerField()
+    level = models.SmallIntegerField()
+    upgrade_tech = models.OneToOneField('CoolingSystem',
+                                        related_name='downgrade_tech')
+    upgrade_power = models.OneToOneField('CoolingSystem',
+                                         related_name='downgrade_power')
+
+    def __str__(self):
+        return "Cooling system : " + self.description
+
+
+class CPU(models.Model):
+    description = models.TextField()
+    price = models.SmallIntegerField()
+    image = models.ImageField()
+    power_max = models.SmallIntegerField()
+    frequency_max = models.SmallIntegerField()
+    core_count = models.SmallIntegerField()
+    upgrade_freq = models.OneToOneField('CPU',
+                                        related_name='downgrade_freq')
+    upgrade_core = models.OneToOneField('CPU',
+                                        related_name='downgrade_core')
+
+    def __str__(self):
+        return "CPU : " + self.description
+
+
+class RAM(models.Model):
+    description = models.TextField()
+    price = models.SmallIntegerField()
+    image = models.ImageField()
+    power_max = models.SmallIntegerField()
+    frequency_max = models.SmallIntegerField()
+    memory_max = models.SmallIntegerField()
+    upgrade_freq = models.OneToOneField('RAM',
+                                        related_name='downgrade_freq')
+    upgrade_memory = models.OneToOneField('RAM',
+                                          related_name='downgrade_memory')
+
+    def __str__(self):
+        return "RAM : " + self.description
+
+
+class HardDrive(models.Model):
+    description = models.TextField()
+    price = models.SmallIntegerField()
+    image = models.ImageField()
+    power_max = models.SmallIntegerField()
+    speed_max = models.SmallIntegerField()
+    storage_max = models.SmallIntegerField()
+    upgrade_storage = models.OneToOneField('HardDrive',
+                                           related_name='downgrade_storage')
+    upgrade_speed = models.OneToOneField('HardDrive',
+                                         related_name='downgrade_speed')
+    upgrade_tech = models.OneToOneField('HardDrive',
+                                        related_name='downgrade_tech')
+
+    def __str__(self):
+        return "Hard drive : " + self.description
+
+
+class NetworkInterface(models.Model):
+    description = models.TextField()
+    price = models.SmallIntegerField()
+    image = models.ImageField()
+    power_max = models.SmallIntegerField()
+    upgrade_bandwidth = models.OneToOneField('NetworkInterface',
+                                             related_name='downgrade_bandwith')
+
+    def __str__(self):
+        return "Network Interface : " + self.description
+
+
